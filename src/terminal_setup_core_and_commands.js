d62548b279c551d685b68bcbe6bea627212c2ad8
Vicayo Zhang <zhang@umass.edu __or__ vicayo.c@gmail.com>
2025-04-11 22:31:06 -0400

    add indexDB for data persistence.

--- src/terminal_setup_core_and_commands.js
+++ src/terminal_setup_core_and_commands.js
@@ -1,7 +1,10 @@
-let terminalCore = undefined;
+let button_to_save_terminal_file_system_to_indexDB = undefined;
 
 document.addEventListener('DOMContentLoaded', () => {
-    terminalCore = generateTerminalCore();
+    const terminalCore = generateTerminalCore();
+
+    // Set Up Button Functions Linking
+    button_to_save_terminal_file_system_to_indexDB = terminalCore.button_to_save_terminal_file_system_to_indexDB;
 
     // Finished
     terminalCore.getSupportedCommands()['help'] = {
@@ -65,44 +68,39 @@ document.addEventListener('DOMContentLoaded', () => {
         description: 'Simply print all the parameters -- with quotation marks [\"] added at the beginning and the end.\n Usage: echo [parameter_sequence]',
     };
 
-    terminalCore.getSupportedCommands()['ls'] = (() => {
-        // Inner Environment
-        const newFolderPointer = terminalCore.createFolderPointer();
-
-        return {
-            executable: (parameters) => {
-                switch (parameters.length) {
-                    case 0: { // print current folder info
-                        terminalCore.printToWindow(`${terminalCore.getCurrentFolderPointer().getContentListAsString()}`, false, true);
-                        break;
-                    }
-                    case 1: { // print the folder info of given path
-                        try {
-                            newFolderPointer.gotoRoot();
-                            parameters[0].split('/').forEach((folderName) => {
-                                if (folderName.length > 0) // maybe meet "/" which yields ""
-                                    newFolderPointer.gotoSubfolder(folderName);
-                            });
-                            terminalCore.printToWindow(`${newFolderPointer.getContentListAsString()}`, false, true);
-                        } catch (e) {
-                            terminalCore.printToWindow(`${e}`, false, true);
-                        }
-                        break;
-                    }
-                    default: {
-                        terminalCore.printToWindow(`Wrong grammar!\nUsage: ls [folder_path]`, false, true);
-                    }
+    // TODO: Update it!!!
+    terminalCore.getSupportedCommands()['ls'] = {
+        executable: (parameters) => {
+            switch (parameters.length) {
+                case 0: { // print current folder info
+                    terminalCore.printToWindow(`${terminalCore.getCurrentFolderPointer().getContentListAsString()}`, false, true);
+                    break;
                 }
-            },
-            description: 'List all the folders and files.\nUsage: ls [folder_path]'
-        };
-    })();
+                case 1: { // print the folder info of given path
+                    try {
+                        const newFolderPointer = terminalCore.getNewFolderPointer();
+                        newFolderPointer.gotoRoot();
+                        newFolderPointer.gotoSubpath(parameters[0]);
+                        terminalCore.printToWindow(`${newFolderPointer.getContentListAsString()}`, false, true);
+                    } catch (e) {
+                        terminalCore.printToWindow(`${e}`, false, true);
+                    }
+                    break;
+                }
+                default: {
+                    terminalCore.printToWindow(`Wrong grammar!\nUsage: ls [folder_path]`, false, true);
+                }
+            }
+        },
+        description: 'List all the folders and files.\nUsage: ls [folder_path]'
+    };
 
     terminalCore.getSupportedCommands()['mkdir'] = {
         executable: (parameters) => {
-
+            if (parameters.length !== 1)
+                return;
         },
-        description: ''
+        description: 'Make a new directory.\nUsage: mkdir [folder_name/folder_path]'
     };
 
     terminalCore.getSupportedCommands()['touch'] = {
@@ -133,8 +131,6 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         description: ''
     };
-
-
 
 
 });
